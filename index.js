89a0cae352c0b85c7e589aba35289762d2112455
multipletwigs <starlaxone@gmail.com>
2022-04-20 23:01:25 +0800

    Added in some minor fixes with some js code

---
+++ index.js
@@ -0,0 +1,5 @@
+const reset = document.getElementById("save-btn")
+const buttons = document.querySelector("button")
+reset.addEventListener("click", (event)=>{
+    console.log(event.target.innerHTML)
+})
